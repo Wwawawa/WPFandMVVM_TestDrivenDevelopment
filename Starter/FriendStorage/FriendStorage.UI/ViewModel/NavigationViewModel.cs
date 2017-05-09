0223bdc224a97e348941e4cce7f6b4df156475ab
Wwawawa <421299099@qq.com>
2017-05-09 17:55:56 +0800

    Spike

--- Starter/FriendStorage/FriendStorage.UI/ViewModel/NavigationViewModel.cs
+++ Starter/FriendStorage/FriendStorage.UI/ViewModel/NavigationViewModel.cs
@@ -1,7 +1,26 @@
-﻿namespace FriendStorage.UI.ViewModel
-{
-  public class NavigationViewModel : ViewModelBase
-  {
+using System;
+using FriendStorage.DataAccess;
+using System.Collections.ObjectModel;
+using FriendStorage.Model;
 
-  }
+namespace FriendStorage.UI.ViewModel
+{
+    public class NavigationViewModel : ViewModelBase
+    {
+        public NavigationViewModel()
+        {
+            Friends = new ObservableCollection<Friend>();
+        }
+        public void Load()
+        {
+            var dataService = new FileDataService();
+            foreach(var friend in dataService.GetAllFriends())
+            {
+                Friends.Add(friend);
+            }
+        }
+
+        public ObservableCollection<Friend> Friends { get; private set; }
+    }
 }
+
