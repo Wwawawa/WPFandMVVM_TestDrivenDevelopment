bb8634eae5438246578e41bab7931e6485d28bb1
Wwawawa <421299099@qq.com>
2017-05-09 17:46:01 +0800

    Spike

--- Starter/FriendStorage/FriendStorage.UI/App.xaml.cs
+++ Starter/FriendStorage/FriendStorage.UI/App.xaml.cs
@@ -4,6 +4,11 @@ namespace FriendStorage.UI
 {
   public partial class App : Application
   {
-
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var mainWindow = new MainWindow(new MainViewModel());
+            mainWindow.Show();
+        }
   }
 }
