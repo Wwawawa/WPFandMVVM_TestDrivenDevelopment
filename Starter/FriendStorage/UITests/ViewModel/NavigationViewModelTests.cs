31d3bd50f8f176fa198f0e43a79f1cd62c7d05da
Wwawawa <421299099@qq.com>
2017-05-11 15:39:58 +0800

    Update NavigationViewModelTests.cs

--- Starter/FriendStorage/UITests/ViewModel/NavigationViewModelTests.cs
+++ Starter/FriendStorage/UITests/ViewModel/NavigationViewModelTests.cs
@@ -1,6 +1,46 @@
+using FriendStorage.UI.ViewModel;
+using Xunit;
+using FriendStorage.Model;
+using FriendStorage.UI.DataProvider;
+using System.Linq;
+using System.Collections.Generic;
+
+
 namespace UITests.ViewModel
 {
     public class NavigationViewModelTests
     {
+        [Fact]
+        public void ShouldLoadFriends()
+        {
+            var viewModel = new NavigationViewModel(new NavigationDataProviderMock());
+            viewModel.Load();
+            Assert.Equal(2, viewModel.Friends.Count);
+
+            var friend = viewModel.Friends.SingleOrDefault(f => f.Id == 1);
+            Assert.NotNull(friend);
+            Assert.Equal("Julia", friend.FirstName);
+
+            friend = viewModel.Friends.SingleOrDefault(f => f.Id == 2);
+            Assert.NotNull(friend);
+            Assert.Equal("Thomas", friend.FirstName);
+        }
+        [Fact]
+        public void ShouldLoadFriendsOnlyOnce()
+        {
+            var viewModel = new NavigationViewModel(new NavigationDataProviderMock());
+            viewModel.Load();
+            viewModel.Load();
+            Assert.Equal(2, viewModel.Friends.Count);
+        }
+    }
+    public class NavigationDataProviderMock : INavigationDataProvider
+    {
+        public IEnumerable<Friend> GetAllFriends()
+        {
+            yield return new Friend { Id = 1, FirstName = "Julia" };
+            yield return new Friend { Id = 2, FirstName = "Thomas" };
+        }
     }
 }
+
