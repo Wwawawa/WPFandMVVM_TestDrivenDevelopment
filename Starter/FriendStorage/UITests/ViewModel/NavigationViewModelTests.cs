6f0cc75233962cdb1d0ae59021b7a6f03428c228
Wwawawa <421299099@qq.com>
2017-05-11 17:37:04 +0800

    Update NavigationViewModelTests.cs

--- Starter/FriendStorage/UITests/ViewModel/NavigationViewModelTests.cs
+++ Starter/FriendStorage/UITests/ViewModel/NavigationViewModelTests.cs
@@ -19,11 +19,11 @@ namespace UITests.ViewModel
 
             var friend = viewModel.Friends.SingleOrDefault(f => f.Id == 1);
             Assert.NotNull(friend);
-            Assert.Equal("Julia", friend.FirstName);
+            Assert.Equal("Julia", friend.DisplayMember);
 
             friend = viewModel.Friends.SingleOrDefault(f => f.Id == 2);
             Assert.NotNull(friend);
-            Assert.Equal("Thomas", friend.FirstName);
+            Assert.Equal("Thomas", friend.DisplayMember);
         }
         [Fact]
         public void ShouldLoadFriendsOnlyOnce()
@@ -36,11 +36,10 @@ namespace UITests.ViewModel
     }
     public class NavigationDataProviderMock : INavigationDataProvider
     {
-        public IEnumerable<Friend> GetAllFriends()
+        public IEnumerable<LookupItem> GetAllFriends()
         {
-            yield return new Friend { Id = 1, FirstName = "Julia" };
-            yield return new Friend { Id = 2, FirstName = "Thomas" };
+            yield return new LookupItem { Id = 1, DisplayMember = "Julia" };
+            yield return new LookupItem { Id = 2, DisplayMember = "Thomas" };
         }
     }
 }
-
