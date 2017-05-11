f8a7c237941af0447c7273016bccf6c592d767fb
Wwawawa <421299099@qq.com>
2017-05-11 18:23:00 +0800

    Create MainViewModelTest.cs

---
+++ Starter/FriendStorage/UITests/ViewModel/MainViewModelTest.cs
@@ -0,0 +1,16 @@
+
+using FriendStorage.UI.ViewModel;
+using Xunit;
+
+namespace UITests.ViewModel
+{
+    public class MainViewModelTest
+    {
+        [Fact]
+        public void ShouldCallLoadMethodOfNavigationViewModel()
+        {
+            var viewModel = new MainViewModel();
+
+        }
+    }
+}
