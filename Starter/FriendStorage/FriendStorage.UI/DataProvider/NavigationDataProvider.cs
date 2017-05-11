932af0cebe604b0267ba25e4111edf904b2ccbdb
Wwawawa <421299099@qq.com>
2017-05-11 16:58:48 +0800

    Create NavigationDataProvider.cs
    Here we deployed a Func<> method in order to call the Idispose of IdataService

---
+++ Starter/FriendStorage/FriendStorage.UI/DataProvider/NavigationDataProvider.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using FriendStorage.DataAccess;
+using FriendStorage.Model;
+
+namespace FriendStorage.UI.DataProvider
+{
+    public class NavigationDataProvider : INavigationDataProvider
+    {
+        private Func<IDataService> _dataServiceCreator;
+
+        public NavigationDataProvider(Func<IDataService> dataServiceCreator)
+        {
+            _dataServiceCreator = dataServiceCreator;
+        }
+        public IEnumerable<Friend> GetAllFriends()
+        {
+            using(var dataService = _dataServiceCreator())
+            {
+                return dataService.GetAllFriends();
+            }            
+        }
+    }
+}
