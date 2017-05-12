8197ff0332be2a33a570627d3aba85d52198f70a
Wwawawa <421299099@qq.com>
2017-05-12 09:38:07 +0800

    Update App.xaml.cs

--- Starter/FriendStorage/FriendStorage.UI/App.xaml.cs
+++ Starter/FriendStorage/FriendStorage.UI/App.xaml.cs
@@ -1,14 +1,21 @@
-﻿using System.Windows;
+using System.Windows;
+using FriendStorage.UI.DataProvider;
+using FriendStorage.UI.View;
+using FriendStorage.UI.ViewModel;
 
 namespace FriendStorage.UI
 {
-  public partial class App : Application
-  {
+    public partial class App : Application
+    {
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            var mainWindow = new MainWindow(new MainViewModel());
+            var mainWindow = new MainWindow(
+                new MainViewModel(
+                    new NavigationViewModel(
+                        new NavigationDataProvider(
+                            () => new DataAccess.FileDataService()))));
             mainWindow.Show();
         }
-  }
+    }
 }
