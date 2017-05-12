f894691e66e8bdb38c2dad21c4f9df385a569fa5
Wwawawa <421299099@qq.com>
2017-05-12 16:21:02 +0800

    Update NavigationViewModelTests.cs

--- Starter/FriendStorage/UITests/ViewModel/NavigationViewModelTests.cs
+++ Starter/FriendStorage/UITests/ViewModel/NavigationViewModelTests.cs
@@ -5,6 +5,7 @@ using FriendStorage.UI.DataProvider;
 using System.Linq;
 using System.Collections.Generic;
 using Moq;
+using Prism.Events;
 
 namespace UITests.ViewModel
 {
@@ -14,6 +15,7 @@ namespace UITests.ViewModel
 
         public NavigationViewModelTests()
         {
+            var eventAggregatorMock = new Mock<IEventAggregator>();
             var navigationDataProviderMock = new Mock<INavigationDataProvider>();
             navigationDataProviderMock.Setup(dp => dp.GetAllFriends()).Returns(
                 new List<LookupItem>()
@@ -22,7 +24,7 @@ namespace UITests.ViewModel
                      new LookupItem { Id = 2, DisplayMember = "Thomas" }
                 });
 
-            _viewModel = new NavigationViewModel(navigationDataProviderMock.Object);
+            _viewModel = new NavigationViewModel(navigationDataProviderMock.Object, eventAggregatorMock.Object);
         }
         [Fact]
         public void ShouldLoadFriends()
@@ -47,4 +49,3 @@ namespace UITests.ViewModel
         }
     }    
 }
-
