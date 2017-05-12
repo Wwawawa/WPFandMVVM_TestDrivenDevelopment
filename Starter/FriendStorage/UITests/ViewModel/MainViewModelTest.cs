80bf777c9991bf86fa389bf082548fb666ad3aff
Wwawawa <421299099@qq.com>
2017-05-12 14:03:49 +0800

    Update MainViewModelTest.cs

--- Starter/FriendStorage/UITests/ViewModel/MainViewModelTest.cs
+++ Starter/FriendStorage/UITests/ViewModel/MainViewModelTest.cs
@@ -1,29 +1,26 @@
 
 using System;
 using FriendStorage.UI.ViewModel;
+using Moq;
 using Xunit;
 
 namespace UITests.ViewModel
 {
     public class MainViewModelTest
     {
+        private Mock<INavigationViewModel> _navigationViewModelMock;
+        private MainViewModel _viewModel;
+
+        public MainViewModelTest()
+        {
+            _navigationViewModelMock = new Mock<INavigationViewModel>();
+            _viewModel = new MainViewModel(_navigationViewModelMock.Object);
+        }
         [Fact]
         public void ShouldCallLoadMethodOfNavigationViewModel()
         {
-            var navigationViewModelMock = new NavigationViewModelMock();
-            var viewModel = new MainViewModel(navigationViewModelMock);
-
-            viewModel.Load();
-
-            Assert.True(navigationViewModelMock.LoadHasBeenCalled);
-        }
-    }
-    public class NavigationViewModelMock : INavigationViewModel
-    {
-        public bool LoadHasBeenCalled { get; set; }
-        public void Load()
-        {
-            LoadHasBeenCalled = true;
+            _viewModel.Load();
+            _navigationViewModelMock.Verify(vm=>vm.Load(), Times.Once);
         }
     }
 }
