a039c9b5979b17679fb235da5eb19c2bf25aca76
Wwawawa <421299099@qq.com>
2017-05-12 18:05:24 +0800

    Update MainViewModel.cs

--- Starter/FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
+++ Starter/FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
@@ -1,16 +1,33 @@
+using System.Collections.ObjectModel;
 using FriendStorage.UI.DataProvider;
 
 namespace FriendStorage.UI.ViewModel
 {
   public class MainViewModel : ViewModelBase
   {
+        private FriendEditViewModel _selectedFriendEditViewModel;
         public MainViewModel(INavigationViewModel navigationViewModel)
         {
-            NavigationViewModel = navigationViewModel;                
+            NavigationViewModel = navigationViewModel;
+            FriendEditViewModels = new ObservableCollection<FriendEditViewModel>();                
         }
 
         public INavigationViewModel NavigationViewModel { get; private set; }
 
+        public ObservableCollection<FriendEditViewModel> FriendEditViewModels { get; set; }
+
+        public FriendEditViewModel SelectedFriendEditViewModel
+        {
+            get
+            {
+                return _selectedFriendEditViewModel;
+            }
+
+            set
+            {
+                _selectedFriendEditViewModel = value;
+            }
+        }        
         public void Load()
         {
             NavigationViewModel.Load();
