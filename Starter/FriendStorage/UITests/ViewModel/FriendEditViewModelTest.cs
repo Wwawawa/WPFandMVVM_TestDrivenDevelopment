905ef6774ebe6400ed233b80146d6df974d499f1
Wwawawa <421299099@qq.com>
2017-05-15 17:37:41 +0800

    Create FriendEditViewModelTest.cs

---
+++ Starter/FriendStorage/UITests/ViewModel/FriendEditViewModelTest.cs
@@ -0,0 +1,48 @@
+using FriendStorage.Model;
+using FriendStorage.UI.DataProvider;
+using FriendStorage.UI.Events;
+using FriendStorage.UI.ViewModel;
+using UITests.Extensions;
+using Moq;
+using Prism.Events;
+using Xunit;
+
+namespace FriendStroage.UITests.ViewModel
+{
+    public class FriendEditViewModelTest
+    {
+        private const int _friendId = 5;
+        private Mock<IFriendDataProvider> _dataProviderMock;
+        private FriendEditViewModel _viewModel;
+
+        public FriendEditViewModelTest()
+        {
+            _dataProviderMock = new Mock<IFriendDataProvider>();
+            _dataProviderMock.Setup(dp => dp.GetFriendById(_friendId))
+                .Returns(new Friend() { Id = _friendId, FirstName = "Thomas" });         
+
+            _viewModel = new FriendEditViewModel(_dataProviderMock.Object);
+        }
+
+        [Fact]
+        public void ShouldLoadFriend()
+        {
+            _viewModel.Load(_friendId);
+
+            Assert.NotNull(_viewModel.Friend);
+            Assert.Equal(_friendId, _viewModel.Friend.Id);
+
+            _dataProviderMock.Verify(dp => dp.GetFriendById(_friendId), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldRaisePropertyChangedEventForFriend()
+        {
+            var fired = _viewModel.IsPropertyChangedFired(
+                () => _viewModel.Load(_friendId),
+                nameof(_viewModel.Friend));
+
+            Assert.True(fired);
+        }       
+    }
+}
