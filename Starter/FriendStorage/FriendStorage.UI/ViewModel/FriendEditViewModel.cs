f4a52d5c8d5dc15022c215a0e5a44ab2e8afa253
Wwawawa <421299099@qq.com>
2017-05-15 14:05:55 +0800

    Add-FriendProperty

--- Starter/FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
+++ Starter/FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
@@ -1,13 +1,23 @@
 using System;
+using FriendStorage.Model;
 
 namespace FriendStorage.UI.ViewModel
 {
     public interface IFriendEditViewModel
     {
         void Load(int friendId);
+        Friend Friend { get; }
     }
     public class FriendEditViewModel : ViewModelBase, IFriendEditViewModel
     {
+        public Friend Friend
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         public void Load(int friendId)
         {
             throw new NotImplementedException();
